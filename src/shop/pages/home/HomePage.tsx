c5b9bb10cbb7124b9f2a3bc48637d001ad920d97
Jonas Martinez <jonasmartinez0126@gmail.com>
2025-12-23 18:05:02 -0300

    Se inicio el proyecto de la aplicacion teslo shop, solo la parte del diseño hasta el momento

---
+++ src/shop/pages/home/HomePage.tsx
@@ -0,0 +1,14 @@
+import { CustomPagination } from "@/components/custom/CustomPagination"
+import { CustomJumbotron } from "@/shop/components/CustomJumbotron"
+import { ProductsGrid } from "@/shop/components/ProductsGrid"
+import { products } from "@/mocks/products.mock"
+
+export const HomePage = () => {
+    return (
+        <>
+            <CustomJumbotron title="Todos los productos" />
+            <ProductsGrid products={products} />
+            <CustomPagination totalPages={5} />
+        </>
+    )
+}
